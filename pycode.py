b1f57fe8cbc6d05d9e5e4983aa2a76d3c04fb50b
billedx <bill.sawarell@gmail.com>
2018-09-18 07:38:37 +0200

    added the first code

---
+++ pycode.py
@@ -0,0 +1,10 @@
+# Adding first function, adds two numbers together
+
+def addingNumbers(a, b):
+    c = a + b
+    return c
+
+a = 5
+b = 10
+c = addingNumbers(a, b)
+print("My funtion returns the value", c)
